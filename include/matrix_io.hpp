0f48cd8195ef0e0751eb288eacf0baec43785b48
Valeriia <valery_potrebina_2003@mail.ru>
2025-10-11 16:36:40 +0300

    Add library matrix IO

--- include/matrix_io.hpp
+++ include/matrix_io.hpp
@@ -8,10 +8,15 @@
 namespace MtxIO {
     bool load_matrix(const std::string &filename,Types::Sparse &matrix);
     bool load_vector(const std::string &filename,Types::Vector &matrix);
+    bool save_matrix(const std::string &filename,const Types::Sparse &matrix);
+    bool save_vector(const std::string &filename,const Types::Vector &vector);
 };
 
 namespace LisIO {
     bool load_matrix(const std::string &filename,Types::Sparse &matrix);
+    bool load_vector(const std::string &filename,Types::Vector &matrix);
+    bool save_matrix(const std::string &filename,const Types::Sparse &matrix);
+    bool save_vector(const std::string &filename,const Types::Vector &vector);
 }
 
 #endif //CHALLENGE2_MATRIX_IO_HPP
