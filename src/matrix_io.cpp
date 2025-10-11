dc65f86b640fd4af6e1dd38e8efb602ed8407d46
Valeriia <valery_potrebina_2003@mail.ru>
2025-10-11 18:08:08 +0300

    Implement functions for matrix io library

--- src/matrix_io.cpp
+++ src/matrix_io.cpp
@@ -1,5 +1,18 @@
 #include "../include/matrix_io.hpp"
 
-bool MtxIO::load_matrix(const std::string &filename, Ch2Traits::Sparse &matrix) {
-    return loadMarket( matrix,filename);
+
+bool MtxIO::load_matrix(const std::string &filename, Types::Sparse &matrix) {
+    return loadMarket(matrix, filename);
+}
+
+bool MtxIO::load_vector(const std::string &filename, Types::Vector &vector) {
+    return loadMarketVector(vector, filename);
+}
+
+bool MtxIO::save_matrix(const std::string &filename, const Types::Sparse &matrix) {
+    return saveMarket(matrix, filename);
+}
+
+bool MtxIO::save_vector(const std::string &filename, const Types::Vector &vector) {
+    return saveMarketVector(vector, filename);
 }
