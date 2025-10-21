208634986556e009f61989b2dfa22fac1e71b738
Vale@Luna <vthvw02@gmail.com>
2025-10-21 11:02:22 +0200

    fix things

--- src/task2.cpp
+++ src/task2.cpp
@@ -31,9 +31,10 @@ int main(int argc, char *argv[]) {
     auto eval = eigensolver.eigenvalues();
     auto evec = eigensolver.eigenvectors();
 
+    // std::cout << eval.transpose() << std::endl;
     std::cout << eval.minCoeff() << std::endl;
     std::cout << eval.maxCoeff() << std::endl;
-    std::cout << eval << std::endl;
+    // std::cout << evec.col(1).transpose() << std::endl;
 
     return 0;
 }
